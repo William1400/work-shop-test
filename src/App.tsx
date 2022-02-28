0bfa01ce60964ffdfd54eb00fa97ad0600825405
unknown <william.bady.1400@gmail.com>
2022-02-28 09:50:39 +0100

    initialize test-repo

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// App.tsx fait partie du build folder
+import React/* , { useState }  */from 'react';
+
+// Components
+
+// Types 
+
+// Styles
+
+function App() {
+
+    return (
+
+        <>
+            <div className="App">
+                Welcome
+            </div>
+        </>
+    );
+}
+
+export default App;
+
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,18 @@
+// Components/QuestionCard.tsx
+
+import React from 'react';
+
+// types
+
+// styles
+
+// type
+
+const QuestionCard = () => (
+
+    <div>
+    QuestionCard
+    </div>
+);
+
+export default QuestionCard;
